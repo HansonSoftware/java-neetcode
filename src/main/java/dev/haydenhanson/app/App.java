0abe87782b41b6eadd0d93819a6dbc898368fb6b
Hayden <haydenhansondev+GitHub@gmail.com>
2024-08-11 09:16:17 -0400

    init: initial commit
    - repository generated with mvn archetype:generate
    - TDD w/ JUnit

---
+++ src/main/java/dev/haydenhanson/app/App.java
@@ -0,0 +1,11 @@
+package dev.haydenhanson.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
